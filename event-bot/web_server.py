cc84abb41050d4b9d203e962cc8ce083f54cd143
Hubert <behappyccc@gmail.com>
2018-10-26 15:40:18 +0800

    Update event-bot

--- event-bot/web_server.py
+++ event-bot/web_server.py
@@ -27,13 +27,19 @@ class Webhook(Resource):
 
     def post(self):
         data = request.get_json(force=True)
+        logging.info(data)
         if data['object'] == 'page':
             for entry in data['entry']:
                 webhook_event = entry['messaging'][0]
                 sender_psid = webhook_event['sender']['id']
                 if 'message' in webhook_event:
-                    message = webhook_event['message']
-                    handle_message(sender_psid, message)
+                    if 'quick_reply' in webhook_event['message']:
+                        if 'payload' in webhook_event['message']['quick_reply']:
+                            postback = webhook_event['message']['quick_reply']
+                            handle_postback(sender_psid, postback)
+                    else:
+                        message = webhook_event['message']
+                        handle_message(sender_psid, message)
                 elif 'postback' in webhook_event:
                     postback = webhook_event['postback']
                     handle_postback(sender_psid, postback)
@@ -46,6 +52,7 @@ class InitBot(Resource):
     def post(self):
         set_welcome_screen()
         set_persistent_menu()
+        return {"message": "success"}
 
 
 def call_send_api(sender_psid, response):
@@ -56,6 +63,7 @@ def call_send_api(sender_psid, response):
 
     resp = requests.post(API_URL + API_VERSION +
                          "/me/messages?access_token=" + PAGE_ACCESS_TOKEN, json=data)
+    print(resp.text)
 
 
 def set_welcome_screen():
@@ -64,12 +72,13 @@ def set_welcome_screen():
         "greeting": [
             {
             "locale":"default",
-            "text":"Hello!" 
+            "text":"Facebook Messenger 聊天機器人實作班" 
             }
         ]
     }
     resp = requests.post(API_URL + API_VERSION +
                          "/me/messenger_profile?access_token=" + PAGE_ACCESS_TOKEN, json=data)
+    
     if resp.status_code > 300:
         logging.error('Unable to set welcome screen :' + resp.text)
 
@@ -82,14 +91,19 @@ def set_persistent_menu():
                 'locale':'default',
                 'call_to_actions': [
                     {
-                        "title":"Menu",
+                        "title":"主選單",
                         'type': 'postback',
                         'payload': 'menu'
                     },
                     {
-                        'title': 'website',
+                        'title': '購票',
                         'type': 'web_url',
                         'url': 'https://tenlong.kktix.cc/events/facebook-messenger-chatbot-101'
+                    },
+                    {
+                        'title': '松果城市',
+                        'type': 'web_url',
+                        'url': 'https://www.pycone.com/'
                     }
                 ]
             }
@@ -112,9 +126,8 @@ def handle_message(sender_psid, received_message):
 
 def handle_postback(sender_psid, received_postback):
     payload = received_postback['payload']
-    payload_action = False
     if (payload == 'greeting'):
-        response = {"text": "Hello!"}
+        response = reply_postback_greeting()
     elif (payload == 'introduction'):
         response = reply_postback_introduction()
     elif (payload == 'agenda'):
@@ -132,9 +145,15 @@ def handle_postback(sender_psid, received_postback):
 
     call_send_api(sender_psid, response)
 
+def reply_postback_greeting():
+    data = {
+        "text": "Facebook Messenger 聊天機器人實作班 － 手把手教你用 Python 建立自己的第一個聊天機器人"
+    }
+    return data
+
 def reply_postback_menu():
     data = {
-        "text": "Facebook Messenger 聊天機器人實作班 － 手把手教你用 Python 建立自己的第一個聊天機器人",
+        "text": "主選單",
         "quick_replies":[
             {
                 "content_type":"text",
@@ -176,27 +195,107 @@ def reply_postback_menu():
     return data
 
 def reply_postback_introduction():
-    data = {"text": "Hello!"}
+    data = {"text": "Facebook Messenger 聊天機器人實作班－手把手教你用 Python 建立自己的第一個聊天機器人\n 上完課後你可以學到從規劃到實作，建置專屬的聊天機器人"}
     return data
 
 def reply_postback_agenda():
-    data = {"text": "Hello!"}
+    data = {
+        "attachment": {
+            "payload": {
+                "elements": [
+                {
+                    "image_url": "https://images.unsplash.com/photo-1527430253228-e93688616381",
+                    "subtitle": "講解實作聊天機器的底層概念－Webhook、還有底層技術－Web Server、Facebook 應用程式設定和開發者工具等等，讓你打穩Messenger聊天機器人的基礎。\n 時間: 9:30-12:30",
+                    "title": "聊天機器人基石"
+                },
+                {
+                    "image_url": "https://images.unsplash.com/photo-1533915828531-55b274d98dc5",
+                    "subtitle": "告訴你如何發送精美的範本訊息，以及像是群體推播、貼文留言回覆等進階功能，各種聊天機器人的運用實例，透過豐富範例教你打造聊天機器人。\n 時間: 13:30-16:30",
+                    "title": "聊天機器人技法"
+                }
+                ],
+                "template_type": "generic"
+            },
+            "type": "template"
+        }
+    }
     return data
 
 def reply_postback_traffic_information():
-    data = {}
+    data = {
+        "attachment": {
+            "payload": {
+                "buttons": [
+                {
+                    "messenger_extensions": "true",
+                    "title": "Maps",
+                    "type": "web_url",
+                    "url": "https://behappycc.github.io/pugbot/tenlong-map.html",
+                    "webview_height_ratio": "tall"
+                }
+                ],
+                "template_type": "button",
+                "text": "天瓏資訊圖書 Coding Space 台北市重慶南路一段105號2F \n 捷運台大醫院2號出口，步行五分鐘"
+            },
+            "type": "template"
+        }
+    }
     return data
 
-def reply_postback_traffic_ticket():
-    data = {}
+def reply_postback_ticket():
+    data = {
+        "attachment": {
+            "payload": {
+                "elements": [
+                {
+                    "buttons": [
+                    {
+                        "title": "前往",
+                        "type": "web_url",
+                        "url": "https://tenlong.kktix.cc/events/facebook-messenger-chatbot-101"
+                    }
+                    ],
+                    "image_url": "https://t.kfs.io/upload_images/87747/20181009-_________big_large.jpg",
+                    "subtitle": "早鳥優惠價 1,800元",
+                    "title": "Facebook Messenger 聊天機器人實作班"
+                }
+                ],
+                "template_type": "generic"
+            },
+            "type": "template"
+        }
+    }
     return data
 
-def reply_postback_traffic_sponsor():
-    data = {}
+def reply_postback_sponsor():
+    data = {
+        "attachment": {
+            "payload": {
+                "elements": [
+                {
+                    "buttons": [
+                    {
+                        "title": "前往",
+                        "type": "web_url",
+                        "url": "https://www.tenlong.com.tw/"
+                    }
+                    ],
+                    "image_url": "https://modernweb.tw/img/logo-ten.jpg",
+                    "title": "特別感謝-天瓏資訊圖書"
+                }
+                ],
+                "template_type": "generic"
+            },
+            "type": "template"
+        }
+    }
     return data
 
-def reply_postback_traffic_contact_us():
-    data = {}
+def reply_postback_contact_us():
+    data = {
+              "text": "信箱：steven@tenlong.com.tw"
+    }
+
     return data
 
 
